f9dd09f91816afb4eae869429e16e3e5a3a1ea89
kplachecki <kamil.plachecki@gmail.com>
2019-05-27 12:38:22 +0200

    logo added

--- src/containers/Screens/About/About.js
+++ src/containers/Screens/About/About.js
@@ -1,53 +1,13 @@
 import React from "react";
 import classes from "./About.module.css";
+import logo from "./../../../assets/Logo/LOGO-1.png";
 
 const about = () => (
   <React.Fragment>
     <div id="about" className={classes.body}>
-      <h1>Basic demo</h1>
-      <p>
-        Deserunt sunt et quis est non eu elit aliqua sint mollit. Non elit
-        fugiat ea adipisicing laborum nulla aliqua magna. Duis sint aliqua
-        dolore cillum esse dolore occaecat nisi proident officia in veniam sint
-        consectetur. Ad consectetur occaecat duis Lorem sunt labore non cillum
-        labore dolor ipsum anim dolore. Cillum esse excepteur fugiat fugiat
-        veniam sunt veniam tempor voluptate ipsum incididunt aliquip irure
-        adipisicing. Id incididunt sit culpa cupidatat. Non enim non dolor
-        incididunt cillum cupidatat officia ipsum. Et ut eu occaecat elit labore
-        amet. Ex est eu aliquip ex veniam magna enim qui consectetur nisi sunt
-        ut exercitation id. Officia fugiat non incididunt nostrud dolore do
-        officia commodo ut deserunt. Velit reprehenderit occaecat velit sint
-        sint nulla dolor. Aliquip cillum eu nulla consequat do non consectetur
-        fugiat officia consectetur eu laboris fugiat. Esse dolor ullamco anim
-        proident est incididunt laboris deserunt do quis et do aute. Aliqua enim
-        quis proident cillum ex. Enim incididunt nostrud pariatur minim ipsum
-        dolor. Ad est reprehenderit elit voluptate. Pariatur ut est laborum
-        pariatur sit tempor. Est consectetur quis minim anim. Est culpa eu
-        laborum minim veniam nulla. Eiusmod adipisicing fugiat ipsum consequat
-        eu consequat cillum nostrud labore occaecat reprehenderit dolore id
-        pariatur. Et elit qui sint quis fugiat adipisicing qui sunt dolor
-        ullamco incididunt officia excepteur reprehenderit. Sunt incididunt
-        exercitation esse eu et deserunt et labore dolor aliqua.Quis aute
-        deserunt quis pariatur dolore ut fugiat occaecat incididunt ipsum
-        excepteur nulla. Commodo reprehenderit fugiat duis non laborum cupidatat
-        sint voluptate excepteur magna velit irure. Ullamco culpa adipisicing
-        magna nulla esse qui quis nisi eiusmod duis. Cupidatat est incididunt
-        velit irure eiusmod incididunt voluptate non qui. Ea velit irure esse
-        tempor sit cillum occaecat aliquip irure consectetur culpa sunt. Ea
-        nulla qui adipisicing enim elit non consequat amet nisi nisi nulla. Quis
-        reprehenderit amet aliqua eiusmod do amet deserunt sunt qui veniam. Anim
-        amet ipsum nisi ullamco dolore est aute veniam amet quis ipsum culpa ea
-        veniam. Occaecat Lorem consectetur enim reprehenderit elit duis aliquip
-        officia excepteur sint consequat. Officia pariatur velit elit nulla
-        voluptate qui ad mollit incididunt pariatur. Do et magna consequat aute
-        ipsum fugiat do laborum tempor veniam culpa dolor. Velit officia ipsum
-        in do irure nisi id dolore labore fugiat ipsum pariatur. Consequat
-        tempor laborum Lorem cillum qui eiusmod aliquip nulla anim labore
-        occaecat qui sit. Eu enim amet laborum sit sint. Quis id cillum culpa
-        dolore culpa incididunt anim ad ipsum qui tempor in consequat. Tempor
-        fugiat enim enim dolore occaecat aliqua anim minim officia excepteur
-        cupidatat dolore nostrud. Tempor tempor do ea laboris deserunt mollit
-      </p>
+      <div className={classes.content}>
+        <img src={logo} height="70%" width="70%" />
+      </div>
     </div>
   </React.Fragment>
 );
